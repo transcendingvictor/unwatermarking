0e9f7c0946e903d2599dd4f287a3da8fea4d5e2a
VICTOR ABIA <transcendingvictor@gmail.com>
2024-04-29 13:17:23 +0200

    ConvAE architecture

--- models.py
+++ models.py
@@ -1,1 +1,51 @@
 # Where the VAE, the GAN, and the Diffussion Model are defined.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ConvAutoencoder(nn.Module):
+    def __init__(self):
+        super(ConvAutoencoder, self).__init__()
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+        )
+
+        # Bottleneck.
+
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(
+                128, 64, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                64, 32, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                32, 3, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
+            nn.Sigmoid(),  # Use sigmoid if image pixels are normalized between 0 and 1
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        x = F.interpolate(
+            x, size=(500, 500)
+        )  # Ensure output matches input dimensions exactly
+
+        return x
